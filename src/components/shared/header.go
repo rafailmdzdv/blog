bdffff1fcd37e1cc68dd0cceee45a0c6137ac7f3
rafayt323@gmail.com <rafayt323@gmail.com>
2025-07-08 16:05:14 +0300

    add theme switcher

--- src/components/shared/header.go
+++ src/components/shared/header.go
@@ -1,7 +1,9 @@
 package shared
 
-import "fmt"
-import "github.com/maxence-charriere/go-app/v10/pkg/app"
+import (
+	"fmt"
+	"github.com/maxence-charriere/go-app/v10/pkg/app"
+)
 
 type Header struct {
 	app.Compo
@@ -13,6 +15,7 @@ func (h *Header) Render() app.UI {
 	return app.Header().Class("border-b border-gray-200").Body(
 		app.Div().Class("navbar max-w-7xl mx-auto px-6 py-6 flex justify-between items-center").Body(
 			app.Div().Class("flex-1 text-info text-2xl font-bold").Body(app.A().Href("/").Text(fmt.Sprintf("%s.blog", h.title))),
+			app.Div().Body(&ThemeSwap{}),
 			app.Ul().Class("menu menu-horizontal flex-none rounded-box").Body(
 				app.Li().Body(app.A().Href("/about").Class("hover:text-secondary").Text("About")),
 				app.Li().Body(app.A().Href("/posts").Class("hover:text-secondary").Text("Posts")),
